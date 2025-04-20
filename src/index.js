b909bd5b2fd19f040bb1a8be328cfc7fab603a9e
neelam-bind <neelambind200@gmail.com>
2025-04-20 19:03:55 +0530

    setting up the baisc auth routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import expresss from 'express';
 import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
 const app = expresss();
+app.use(expresss.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello Guys! Welcome to leetlab🔥');
+})
+
+app.use('/api/v1/auth',authRoutes);
 
 app.listen(process.env.PORT , () => {
     console.log('server is running on port 8080');
