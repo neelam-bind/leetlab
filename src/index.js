3ba4b259d3effafce35d85f7f2dbab2479b8a053
neelam-bind <neelambind200@gmail.com>
2025-06-11 00:20:12 +0530

    minor fix

--- src/index.js
+++ src/index.js
@@ -37,6 +37,6 @@ app.use('/api/v1/playlist',playlistRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
