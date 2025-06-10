0d334a4a7c4d644c972b3dcaec406d456cb5ccf7
neelam-bind <neelambind200@gmail.com>
2025-06-11 00:25:19 +0530

    some minor fix

--- src/index.js
+++ src/index.js
@@ -15,12 +15,19 @@ dotenv.config();
 const app = express();
 
 
-app.use(
-    cors('*',{
-      origin: ["http://localhost:5173","https://leetlab-frontend-ebon.vercel.app"],
-      credentials: true,
-    })
-  );
+const allowedOrigins = ["http://localhost:5173", "https://leetlab-frontend-ebon.vercel.app"];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
